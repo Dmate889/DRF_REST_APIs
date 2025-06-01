5624ed7322ff0a24b841b5c7e5422bfb1067a484
FireWolfy89 <dmate899@gmail.com>
2025-06-01 21:03:34 +0200

    feat(Game) add Game data model

--- game_renter/base/models.py
+++ game_renter/base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    name = models.CharField(max_length=20)
+    developer = models.CharField(max_length=20)
+    bio = models.TextField(max_length=255)
+    release_year = models.DateTimeField(null = True)
+    is_rented = models.BooleanField(default = False)
+    amount = models.DecimalField(decimal_places=10, max_digits=20)
+
