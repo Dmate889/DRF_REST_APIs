c1a550c421405c71d9e4b440eebdf87f599fb041
FireWolfy89 <dmate899@gmail.com>
2025-06-11 22:06:08 +0200

    fix (Game model) price instead of amount, can be null,blank,by default 0, unique game nam

--- game_renter/base/models.py
+++ game_renter/base/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 # Create your models here.
 class Game(models.Model):
-    name = models.CharField(max_length=20)
-    developer = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
+    developer = models.CharField(max_length=50)
     bio = models.TextField(max_length=255)
     release_year = models.DateTimeField(null = True)
     is_rented = models.BooleanField(default = False)
-    amount = models.DecimalField(decimal_places=10, max_digits=20)
+    price = models.DecimalField(decimal_places=2, max_digits=8, null = True, blank=True, default=0)
     created_by = models.ForeignKey(User, related_name= 'user', on_delete=models.CASCADE, null=True)
 
