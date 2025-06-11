0a94fa1ae0e241243666a35b889aa98cc7a0292d
FireWolfy89 <dmate899@gmail.com>
2025-06-11 07:29:08 +0200

    feat (JWT token) JWT token implementation for auth

--- game_renter/game_renter/settings.py
+++ game_renter/game_renter/settings.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+
+
 """
 Django settings for game_renter project.
 
@@ -38,7 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "base",
-    "rest_framework"
+    "rest_framework",
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +126,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=20)
+
+}
